29a32f1a7315bb942ac53c78566ef2ecbb510e34
natadevm <natihailug@gmail.com>
2026-02-13 10:12:08 +0300

    feat update

---
+++ cbeNati/src/about.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const About = () => {
+  return (
+    <div>
+      <h1>the frist version of app that work in bank reite cheek </h1>
+    </div>
+  );
+};
+
+export default About;
