d0eec3efec297e917d66aaf04b9475b9b9e4e1ee
natadevm <natihailug@gmail.com>
2026-02-13 10:39:28 +0300

    feat fix2

--- cbeNati/src/bank.jsx
+++ cbeNati/src/bank.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Bank() {
   const [account, setAccount] = useState(""); // For account
@@ -136,7 +137,7 @@ function Bank() {
           >
             Open Transaction
           </button>
-          <a href="/about">about</a>
+          <Link to="/about">About</Link>
         </div>
       </div>
     </div>
